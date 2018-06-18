5693312ac9a13a659e1aa3975cb24325350fbdb0
root <root@118810HD011135.ikexpress.com>
2018-06-18 14:43:55 +0200

    laravel + laragon + mysql

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('lastname');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('firstname');
+            $table->string('username');
             $table->rememberToken();
             $table->timestamps();
         });
