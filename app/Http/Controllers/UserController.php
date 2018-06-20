def377f11e025958b3f97afa1beda62723821ef9
xrain19 <eddy.eljaziri@gmail.com>
2018-06-20 11:39:23 +0200

    menage

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,43 +8,11 @@ use App\equipe;
 use Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Http\Request;
 
 
 class UserController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Register Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users as well as their
-    | validation and creation. By default this controller uses a trait to
-    | provide this functionality without requiring any additional code.
-    |
-    */
-
-    use RegistersUsers;
-
-    /**
-     * Where to redirect users after registration.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
     protected function validator(array $data)
     {
         return Validator::make($data, [
@@ -55,12 +23,6 @@ class UserController extends Controller
         ]);
     }
 
-    /**
-     * Create a new user instance after a valid registration.
-     *
-     * @param  array  $data
-     * @return \App\User
-     */
     protected function createUser(Request $request)
     {
         if(Auth::user()->role->name == 'Administrateur')
@@ -82,10 +44,15 @@ class UserController extends Controller
     }
 
     public function showForm() {
-        $rolesEquipes = array();
-        $rolesEquipes['roles'] = Role::all()->sortBy('name');
-        $rolesEquipes['equipes'] = Equipe::all()->sortBy('name');
+        if(Auth::user()->role->name == 'Administrateur') {
+            $rolesEquipes = array();
+            $rolesEquipes['roles'] = Role::all()->sortBy('name');
+            $rolesEquipes['equipes'] = Equipe::all()->sortBy('name');
 
-        return view('register', ['rolesEquipes' => $rolesEquipes]);
+            return view('register', ['rolesEquipes' => $rolesEquipes]);
+        }else
+        {
+            return redirect('/home');
+        }
     }
 }
